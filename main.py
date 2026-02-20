0662fd723cebb40d2f60e92fad0d7f9ce0555d20
emsuvith <suvith.kumar@eminds.ai>
2026-02-20 13:06:33 +0530

    bug in search

--- main.py
+++ main.py
@@ -53,9 +53,6 @@ def binary_search(arr, target):
     right = len(arr) - 1
     while left <= right:
         mid = (left + right) // 2
-        if arr[mid] == target:
-            return mid
-        elif arr[mid] < target:
             left = mid + 1
         else:
             right = mid - 1
