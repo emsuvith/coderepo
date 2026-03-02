82d88a14b64529e45ce0c6e5c19dbc6f992b2df2
emsuvith <suvith.kumar@eminds.ai>
2026-03-02 11:42:39 +0530

    kk

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ def printf(a):
 def add(a, b):
     return a + b
 
-def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-    return arr 
+# def bubble_sort(arr):
+#     n = len(arr)
+#     for i in range(n):
+#         for j in range(0, n - i - 1):
+#             if arr[j] > arr[j + 1]:
+#                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+#     return arr 
 
 def binary_search(arr, target):
     left = 0
