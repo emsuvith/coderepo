674a13c5dcd9e860a11f350590b02c524ce06325
emsuvith <suvith.kumar@eminds.ai>
2026-03-02 14:54:56 +0530

    ijj

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ def greet_and_farewell() -> str:
 def printf(a):
     print(a)
 
-# def add(a, b):
-#     return a + b
+def add(a, b):
+    return a + b
 
-# def bubble_sort(arr):
-#     n = len(arr)
-#     for i in range(n):
-#         for j in range(0, n - i - 1):
-#             if arr[j] > arr[j + 1]:
-#                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-#     return arr 
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr 
 
 def binary_search(arr, target):
     left = 0
