01c433eb4435d8c5c1641d3be9253ffea388b34c
Suvith ekdev <160001198+emsuvith@users.noreply.github.com>
2026-05-18 10:02:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,15 +11,15 @@
 #     print(a)
 
 # def add(a, b):
-#     return a + b
+    return a + b
 
-# # def bubble_sort(arr):
-# #     n = len(arr)
-# #     for i in range(n):
-# #         for j in range(0, n - i - 1):
-# #             if arr[j] > arr[j + 1]:
-# #                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-# #     return arr 
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr 
 
 # def binary_search(arr, target):
 #     # """
