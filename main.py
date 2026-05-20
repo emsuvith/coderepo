58d75dd4a471639eed34a0aeb5aa449b69f7433f
Suvith ekdev <160001198+emsuvith@users.noreply.github.com>
2026-05-20 15:23:49 +0530

    Comment out bubble_sort function code
    Comment out bubble_sort implementation.

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ def add(a, b):
     return a + b
 
 def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n - i - 1):
+    # n = len(arr)
+    # for i in range(n):
+        # for j in range(0, n - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr 
