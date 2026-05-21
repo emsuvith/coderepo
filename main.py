a1b2460acd97c8a96de328129f2bb3b30b735f64
Suvith ekdev <160001198+emsuvith@users.noreply.github.com>
2026-05-21 07:38:39 +0530

    Uncomment bubble sort function
    Uncomment bubble sort implementation.

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ def greet_and_farewell() -> str:
 
 # def bubble_sort(arr):
 #     # n = len(arr)
-#     # for i in range(n):
-#         # for j in range(0, n - i - 1):
-#             if arr[j] > arr[j + 1]:
-#                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-#     return arr 
+    # for i in range(n):
+        # for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr 
 
 def binary_search(arr, target):
     # """
