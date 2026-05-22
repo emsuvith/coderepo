5f297ab2b3c58625594fb5f2b5f08573058e8b5b
Suvith ekdev <160001198+emsuvith@users.noreply.github.com>
2026-05-22 11:02:34 +0530

    Uncommented farewell and sorting functions

--- main.py
+++ main.py
@@ -2,21 +2,21 @@ from utils import greet, farewell
 
 def greet_and_farewell() -> str:
     print(greet("Alice"))
-    # print(farewell("Bob"))
-    # return "done"
+    print(farewell("Bob"))
+    return "done"
 
-# def printf(a):
-#     print(a)
+def printf(a):
+    print(a)
 
 def add(a, b):
     # return a + b
 
 def bubble_sort(arr):
-    # n = len(arr)
-    # for i in range(n):
-    #     for j in range(0, n - i - 1):
-    #         if arr[j] > arr[j + 1]:
-    #             arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr 
 
 def binary_search(arr, target):
